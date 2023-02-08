1a325aaf47c76c6e1ebcad7b9d471a730864680b
wroan92 <Anders-wroldsen@live.com>
2023-02-08 11:08:30 +0100

    Navbar med hamburgermeny

--- script/nav.js
+++ script/nav.js
@@ -1,7 +1,7 @@
 const hamburgerBtn = document.querySelector('#hamburgerBtn');
-const dropdown = document.querySelector('#dropdownMenu');
+const dropdown = document.querySelector('.dropdown');
 
 
 hamburgerBtn.addEventListener('click', function() {
-  dropdown.classList.toggle('.show');
+  dropdown.classList.toggle('.hide');
 });
