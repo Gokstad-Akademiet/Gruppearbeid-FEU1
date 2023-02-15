50293057f16c76f4b89ea32011698a17e98a78e9
wroan92 <Anders-wroldsen@live.com>
2023-02-15 11:00:53 +0100

    Added forloop and inner html template litteral

--- script/script.js
+++ script/script.js
@@ -66,3 +66,15 @@ const players = [
     image: "",
   },
 ];
+
+for (let i = 0; i < players.length; i++) {
+    document.getElementById("playersContainer").innerHTML += `<div id="playerCards">
+    <h2>${players[i].name}</h2>
+    <h3>${players[i].character}</h3>
+    <p>${players[i].playernumber}</p>
+    <img src="${players[i].image}">
+    </div>`;
+    console.log(players[i].name);
+    console.log(players[i].playernumber);
+    console.log(players[i].image);
+    }
