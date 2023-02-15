1be9c6b3a2fcf877d92c3f894dc5260443e1b67a
maywands <mayo_jan@outlook.com>
2023-02-15 10:19:04 +0100

    laget et liste med objects

--- js/script.js
+++ js/script.js
@@ -16,3 +16,47 @@ function checkTime(i) {
   } //
   return i;
 }
+
+const myobjArray = [ 
+  { 
+    name: "Hugo Oliver Keto",
+    Posisjon: "Keeper",
+    jerseyNumber: 1,
+
+  },
+  {
+    name: "Andreas brede Albertsen",
+    Posisjon: "Keeper",
+    jerseyNumber: 54,
+
+  },
+  {
+    name: "Fredrik Mani Pålerud",
+    Posisjon: "Forsvarspiller",
+    jerseyNumber: 2,
+
+  },
+
+  {
+    name: "Quint Arie Jansen",
+    Posisjon: "Forsvarspiller",
+    jersyNumber: 3,
+
+  },
+  {
+    name: "lan Smeulers",
+    Posisjon: "Forsvarspiller",
+    jersyNumber: 4,
+
+  },
+ {
+  name: "Lara Markmanrud",
+  Posisjon: "Forsvarspiller",
+  jersyNumber: 13,
+ },
+{
+  name: "Jesper Taaje",
+  Posisjon: "Forsvarspiller",
+  jersyNumber: 15,
+}, 
+] 
