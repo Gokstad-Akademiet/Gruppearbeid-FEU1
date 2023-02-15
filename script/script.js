0b322817a98acb0fc7a9d02714a86f7063a1a05e
rubideveloper <rubengrytten@gmail.com>
2023-02-15 10:35:26 +0100

    Made an array with 11 objects, but missing img val

---
+++ script/script.js
@@ -0,0 +1,68 @@
+const players = [
+  {
+    name: "Lars Larsen",
+    character: "Lars Bandasj",
+    playernumber: 14,
+    image: "",
+  },
+  {
+    name: "Kjell Kjellsen",
+    character: "Kjell Propell",
+    playernumber: 10,
+    image: "",
+  },
+  {
+    name: "Anna Andersson",
+    character: "Anna Ballerina",
+    playernumber: 8,
+    image: "",
+  },
+  {
+    name: "Ola Olsen",
+    character: "Ola Dunk",
+    playernumber: 23,
+    image: "",
+  },
+  {
+    name: "Ida Idassen",
+    character: "Ida Måke",
+    playernumber: 17,
+    image: "",
+  },
+  {
+    name: "Per Persen",
+    character: "Per Rakett",
+    playernumber: 7,
+    image: "",
+  },
+  {
+    name: "Maja Majassen",
+    character: "Maja Hopp",
+    playernumber: 11,
+    image: "",
+  },
+  {
+    name: "Anders Andersen",
+    character: "Anders Rask",
+    playernumber: 2,
+    image: "",
+  },
+  {
+    name: "Sara Svensson",
+    character: "Sara Snurr",
+    playernumber: 21,
+    image: "",
+  },
+  {
+    name: "Erik Eriksen",
+    character: "Erik Slalom",
+    playernumber: 18,
+    image: "",
+  },
+  {
+    name: "Ingrid Ingesson",
+    character: "Ingrid Sprint",
+    playernumber: 3,
+    image: "",
+  },
+];
