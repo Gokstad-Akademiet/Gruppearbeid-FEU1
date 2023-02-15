a11c5b425273ea0588179cab427849c860dd9f80
SolgunTT <solgun.tveit@gokstadakademiet.nogit config --global user.namegit config --global user.emailgit config --global user.namegit config --global user.emailgit config --global user.namegit config --global user.name ditt>
2023-02-15 10:26:18 +0100

    opprettet objekter med spiller-info

---
+++ script/om-oss.js
@@ -0,0 +1,13 @@
+const players = [
+  { name: "Hugo Oliver Keto", bilde: "", spillernummer: "1" },
+
+  { name: "Ian Smeulders", bilde: "", spillernummer: "4" },
+
+  { name: "Sander Moen Foss", bilde: "", spillernummer: "17" },
+
+  { name: "Jesper Taaje", bilde: "", spillernummer: "15" },
+
+  { name: "Sander Risan Mørk", bilde: "", spillernummer: "6" },
+  
+  { name: "Youssef Chaib", bilde: "", spillernummer: "11" },
+];
